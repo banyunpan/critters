af1946f3ede83f83dbe694ea13b080ea8d4fed4c
Megan Peregrino <meganperegrino@wireless-10-145-100-98.public.utexas.edu>
2016-10-02 15:04:05 -0500

    added my info

--- Header.java
+++ Header.java
@@ -2,8 +2,8 @@ package assignment4;
 /* CRITTERS <MyClass.java>
  * EE422C Project 4 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
+ * Megan Peregrino
+ * mnp624
  * <Student1 5-digit Unique No.>
  * <Student2 Name>
  * <Student2 EID>
